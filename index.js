3e920ac6d3481a7cc550b6afb66f3b794ec6268a
Max Edmonds <maxedmonds05@gmail.com>
2020-07-03 16:38:24 -0700

    fill in prompt questions

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }, {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    }, {
+        type: 'list',
+        name: "license",
+        choices: ['MIT', 'GNU GPLv3', 'The Unlicense', 'Boost Software License 1.0']
+    }, {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of the project'
+    }, {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+    }, {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.'
+    }, {
+        type: 'input',
+        name: 'contribute',
+        message: 'Provide contribution guidelines for user collaboration.'
+    }, {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide instructions for how to test the project.'
+    }
 
 ];
 
